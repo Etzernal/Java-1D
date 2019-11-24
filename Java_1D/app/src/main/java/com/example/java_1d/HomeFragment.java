beca8a9ba69cf0814ef0884e3afb5b9b95bf5b0d
chup <sherminechua@hotmail.com>
2019-11-24 23:46:46 +0800

    events from shared pref

--- Java_1D/app/src/main/java/com/example/java_1d/HomeFragment.java
+++ Java_1D/app/src/main/java/com/example/java_1d/HomeFragment.java
@@ -3,9 +3,11 @@ package com.example.java_1d;
 import android.app.ActionBar;
 import android.app.usage.UsageEvents;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.drm.DrmStore;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,8 +33,47 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class HomeFragment extends Fragment {
 
+public class HomeFragment extends Fragment {
+    SharedPreferences Event;
+    public void createEvent(){
+        try {
+            Event = this.getActivity().getSharedPreferences("Info", 0);
+        } catch (Exception exception) {
+            Event = null;
+        }
+    }
+
+
+
+    public static String[] eventInfo(SharedPreferences pref) {
+        if(pref!=null) {
+            String[] out=new String[6];
+            String fromDate = pref.getString("fromDate", "");
+            String toDate = pref.getString("toDate", "");
+            out[5]= pref.getString("fromTime", "");
+            out[4]= pref.getString("toTime", "");
+            out[3]= pref.getString("title", "");
+            String fromDateDigits = stripNonDigits(fromDate);
+            out[2] = fromDateDigits.substring(4);
+            out[1] = fromDateDigits.substring(2, 4);
+            out[0]= fromDateDigits.substring(0, 2);
+            return out;
+        }
+        return null;
+
+    }
+
+    public static String stripNonDigits(String input){
+        String out="";
+        for(int i = 0; i < input.length(); i++){
+            final char c = input.charAt(i);
+            if(c > 47 && c < 58){
+                out+=i;
+            }
+        }
+        return out;
+    }
     TextView date_tv;
     TextView event_view;
     SimpleDateFormat dateFormatMonth = new SimpleDateFormat("dd MMMM yyyy", Locale.getDefault());
@@ -44,24 +85,58 @@ public class HomeFragment extends Fragment {
         final TextView event_view = (TextView) view.findViewById(R.id.event_text);
 
 
+
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            String textDisplayed;
+
+            public String[] storeInfo(SharedPreferences pref){
+                if (pref!=null){
+                    String[] info=new String[6];
+                    String fromDate = pref.getString("fromDate", "");
+                    String toDate = pref.getString("toDate", "");
+                    info[1] = pref.getString("fromTime", "");
+                    info[2] = pref.getString("toTime", "");
+                    info[0] = pref.getString("title", "");
+                    String fromDateDigits= stripNonDigits(fromDate);
+                    info[5]=fromDateDigits.substring(4);
+                    info[4]=fromDateDigits.substring(2,4);
+                    info[3]=fromDateDigits.substring(0,2);
+                    return info;
+                }
+                return null;
+            }
+
+
+
+
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String[] info=eventInfo(Event);
+                if (info!=null){
+                    if(String.valueOf(year).equals(info[5]) && String.valueOf(month + 1).equals(info[4])&& String.valueOf(dayOfMonth).equals(info[3])){
+                        textDisplayed= info[0]+":"+info[1]+"-"+info[2];
+                    }
+                else{textDisplayed="";}}
+
                 date_tv.setSingleLine(false);
                 date_tv.setText(String.valueOf(year) + "-" + String.valueOf(month + 1) + "-" + String.valueOf(dayOfMonth)
                         + " \n" );
 //                date_tv.setText(String.valueOf(year) + "-" + String.valueOf(month + 1) + "-" + String.valueOf(dayOfMonth)
 //                        + " \n" );
-                event_view.setText("7.30pm - " + "How to be a Carebear");
+
+
+                    event_view.setText(textDisplayed);
 //                Toast.makeText(getContext(), dayOfMonth + "-" + month + "-" + year, Toast.LENGTH_SHORT).show();
-                calendarView.setVisibility(View.GONE);
-            }
+
+                calendarView.setVisibility(View.GONE); }
         });
+
+
         date_tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 calendarView.setVisibility(calendarView.isShown()
-                ? View.GONE
+                        ? View.GONE
                         : View.VISIBLE);
             }
         });
